19d3417bf61436b2be466497721feebfbf59502c
elmeddinx <xudaverdiyev11@gmail.com>
2024-10-27 16:48:21 +0400

    complated home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,6 +37,138 @@ var swiper = new Swiper(".section-3-swiper", {
         },
     },
 });
+var swiper = new Swiper(".section-4-swiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1.3,
+            spaceBetween: 15,
+        },
+        360: {
+            slidesPerView: 1.5,
+            spaceBetween: 15,
+        },
+        420: {
+            slidesPerView: 1.8,
+            spaceBetween: 20,
+        },
+        520: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+    },
+});
+var swiper = new Swiper(".section-5-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1.3,
+            spaceBetween: 15,
+        },
+        360: {
+            slidesPerView: 1.5,
+            spaceBetween: 15,
+        },
+        420: {
+            slidesPerView: 1.8,
+            spaceBetween: 20,
+        },
+        520: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+    },
+});
+var swiper = new Swiper(".section-6-swiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1.3,
+            spaceBetween: 15,
+        },
+        360: {
+            slidesPerView: 1.5,
+            spaceBetween: 15,
+        },
+        420: {
+            slidesPerView: 1.8,
+            spaceBetween: 20,
+        },
+        520: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+    },
+});
+var swiper = new Swiper(".section-7-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1.3,
+            spaceBetween: 15,
+        },
+        360: {
+            slidesPerView: 1.5,
+            spaceBetween: 15,
+        },
+        420: {
+            slidesPerView: 1.8,
+            spaceBetween: 20,
+        },
+        520: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+    },
+});
 var player = videojs('my-video');
 
 flatpickr("#calendar", {
