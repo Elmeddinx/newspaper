9035802233ce5db66d01c71103b73fe6be710e5a
elmeddinx <xudaverdiyev11@gmail.com>
2024-10-27 15:36:11 +0400

    add tabs menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,40 @@
 var swiper = new Swiper(".big-post-swiper", {
     autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
+        delay: 2500,
+        disableOnInteraction: false,
     },
     loop: true,
     pagination: {
         el: ".swiper-pagination",
+    },
+});
+var swiper = new Swiper(".section-3-swiper", {
+    slidesPerView: 2,
+    spaceBetween: 36,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1.3,
+            spaceBetween: 15,
+        },
+        360: {
+            slidesPerView: 1.5,
+            spaceBetween: 15,
+        },
+        420: {
+            slidesPerView: 1.8,
+            spaceBetween: 20,
+        },
+        520: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
     },
 });
 var player = videojs('my-video');
